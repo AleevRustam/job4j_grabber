67ad8fce61f4ecbede5754277df190d02026aaf2
AleevRustam <re1973@mail.ru>
2024-08-09 12:26:12 +0200

    2.1. Преобразование даты [#289476 #508086]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -7,7 +7,8 @@ public class HabrCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
-            return (parse == null || parse.isEmpty()) ? null : LocalDateTime.parse(parse, formatter);
+        return (parse == null || !parse.matches("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}[+-]\\d{2}:\\d{2}"))
+                ? null
+                : LocalDateTime.parse(parse, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
     }
 }
